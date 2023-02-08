f55b4e148c01d95e12a55c0d3d67480ec5d7cb83
jeunessesept <b.jiacinto@gmail.com>
2023-02-08 17:04:46 +0100

    test server queries

--- config/config.js
+++ config/config.js
@@ -24,4 +24,4 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres'
   }
-}
+};
--- server.mjs
+++ server.mjs
@@ -1,13 +1,29 @@
-import express  from "express";
+import express from "express";
+import Sequelize from "sequelize";
+import bodyParser from "body-parser";
 
 const server = express();
 server.use(express.json());
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true }))
 
 
 server.get("/", (req, res) => {
-    res.status(200).send("<h1> hello welcome to MeliMelo </h1>")
+  res.status(200).send("<h1> hello welcome to MeliMelo </h1>");
+});
+
+server.get('/get/test', (req, res) => { 
+    res.send("<h1> hello, non je ne boude pas</h1>")
+})
+
+server.post('/post/test', (req, res) => {
+    const { name, age } = req.body
+
+    return res.json(`hello i'm ${name}, and i'm ${age}`)
+
+    console.log(`hello i'm ${name}, and i'm ${age}`)
 })
 
 server.listen(3001, () => {
-    console.log("connected")
-})
+  console.log("connected");
+});
