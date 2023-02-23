439912c3b9443c4103c2291913812a7958f3c6a3
fluxnas <>
2023-02-23 11:48:11 +0100

    resolve conflict

--- server.mjs
+++ server.mjs
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import express from 'express'    
-import dotenv from 'dotenv' 
-dotenv.config() 
-
-const server = express()    
-server.use(express.json()) 
-
-
-import Sequelize from 'sequelize' 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  });
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connexion à la base de données établie avec succès.");
-  })
-  .catch((err) => {
-    console.error("Impossible de se connecter à la base de données : ", err);
-  });
-
-  console.log(process.env)
-
-  
-const DB_PORT = process.env.DB_PORT
- server.listen(DB_PORT, () => {console.log(`server connected on ${DB_PORT}`)}) 
-=======
 import express from "express";
 import bodyParser from "body-parser";
 import logger from "morgan"
@@ -63,4 +34,3 @@ server.get("/homepage", getTextInfos)
 server.listen(3001, async () => {
   console.log("connected");
 });
->>>>>>> f8cc4032039b33de1ab1b6e102a3490894b2f9ae
