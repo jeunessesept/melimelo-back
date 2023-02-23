55c1a4bc76c2814dbd27062016bc0a531bf4e082
jeunessesept <b.jiacinto@gmail.com>
2023-02-23 15:22:09 +0100

    23/02 commit

--- server.mjs
+++ server.mjs
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import logger from "morgan"
-import jwtAuthentification from "./middleware/auth.mjs";
+import jwtAuthentification from "./middleware/auth.mjs"
+
 
 import pkg from './controllers/userController.js';
 const { register, login, logout} = pkg;
@@ -15,10 +16,14 @@ const {  postTextLoggedIn,
 const server = express();
 
 ///middleware
+server.use( express.static('public'));
 server.use(express.json());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(logger('common'))
+
+
+
 
 /// user routes
 server.post("/user/register", register);
