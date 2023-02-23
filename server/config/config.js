e23e54ee3446431bf494d0b3b770eec999163213
jeunessesept <b.jiacinto@gmail.com>
2023-02-23 19:59:52 +0100

    added heroku config info

--- server/config/config.js
+++ server/config/config.js
@@ -18,11 +18,13 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable : process.env.DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
