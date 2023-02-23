95ab72f32f08d023ad579799a76c64ef96b387c5
jeunessesept <b.jiacinto@gmail.com>
2023-02-23 15:22:09 +0100

    23/02 commit

--- server/config/config.js
+++ server/config/config.js
@@ -19,10 +19,10 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: 'postgres'
   }
 };
