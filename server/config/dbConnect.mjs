7f9d541806ed4f438e3e5f6293b3b54828f65b46
jeunessesept <b.jiacinto@gmail.com>
2023-02-23 16:51:39 +0100

    test

--- server/config/dbConnect.mjs
+++ server/config/dbConnect.mjs
@@ -2,34 +2,30 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-
 export const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: "localhost",
-    port: process.env.DB_HOST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
-      }
-    }
+        rejectUnauthorized: false,
+      },
+    },
   }
-  
 );
-
-
 
 export const dbConnect = async () => {
   sequelize
-  .authenticate()
-  .then(() => {
-    console.log(`Database connected to discover`)
-}).catch((err) => {
-    console.log(err)
-})
+    .authenticate()
+    .then(() => {
+      console.log(`Database connected to discover`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
