98663aa7ba7df8eaea6a5ee328af5dd8beba37ba
jeunessesept <b.jiacinto@gmail.com>
2023-02-23 16:56:45 +0100

    test

--- server/models/user.js
+++ server/models/user.js
@@ -10,8 +10,8 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: "localhost",
-    port: process.env.DB_HOST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
   }
 );
