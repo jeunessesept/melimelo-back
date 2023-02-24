df93950c89e532d96946752b8f566a26b47a1845
jeunessesept <b.jiacinto@gmail.com>
2023-02-24 09:58:39 +0100

    changes dbConnect.mjs

--- server/config/dbConnect.mjs
+++ server/config/dbConnect.mjs
@@ -2,24 +2,30 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
+let sequelize;
 
-export const dbConnect = async () => {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+  );
+}
+
+const dbConnect = async () => {
   sequelize
     .authenticate()
     .then(() => {
@@ -29,3 +35,5 @@ export const dbConnect = async () => {
       console.log(err);
     });
 };
+
+export {sequelize, dbConnect}
